6f622f114962db576a738b9617ab2df7252c5eef
CaduUds <eduardo.santos@uds.com.br>
2021-08-20 14:22:00 -0300

    renomeando arquivos

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list/app/app_widget.dart';
 import 'package:todo_list/app/core/database/sqlite_connection_factory.dart';
-import 'package:todo_list/app/repositories/user/user_repository.dart';
-import 'package:todo_list/app/service/user/user_service.dart';
+import 'package:todo_list/app/repositories/user/user_repository_impl.dart';
+import 'package:todo_list/app/service/user/user_service_impl.dart';
+
+import 'repositories/user/user_repository_interface.dart';
+import 'service/user/user_service_interface.dart';
 
 class AppModule extends StatelessWidget {
   AppModule({Key? key}) : super(key: key);
@@ -19,11 +22,11 @@ class AppModule extends StatelessWidget {
           lazy: false,
         ),
         Provider<IUserRepository>(
-          create: (context) => UserRepository(firebaseAuth: context.read()),
+          create: (context) => UserRepositoryImpl(firebaseAuth: context.read()),
         ),
         Provider<IUserService>(
-          create: (context) => UserService(userRepository: context.read()),
-        )
+          create: (context) => UserServiceImpl(userRepository: context.read()),
+        ),
       ],
       child: AppWidget(),
     );
