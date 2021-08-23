5dbecb1986ef8003d603603aca8234dabbc18773
CaduUds <eduardo.santos@uds.com.br>
2021-08-23 14:57:22 -0300

    adicionando rotas da home no app_widget

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list/app/core/database/sqlite_adm_connection.dart';
+import 'package:todo_list/app/core/navigator/app_navigator.dart';
 import 'package:todo_list/app/core/ui/todo_list_ui_config.dart';
 import 'package:todo_list/app/modules/auth/auth_module.dart';
+import 'package:todo_list/app/modules/home/home_module.dart';
 import 'package:todo_list/app/modules/splash/splash_page.dart';
 
 class AppWidget extends StatefulWidget {
@@ -30,11 +32,12 @@ class _AppWidgetState extends State<AppWidget> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: TodoListUiConfig.theme,
       title: "Todo List",
-      initialRoute: '/login',
+      theme: TodoListUiConfig.theme,
+      navigatorKey: AppNavigator.navigatorKey,
       routes: {
         ...AuthModule().routers,
+        ...HomeModule().routers,
       },
       home: SplashPage(),
     );
