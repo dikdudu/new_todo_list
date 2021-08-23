54c3f272950d9036708f62bc322097d976f3af01
CaduUds <eduardo.santos@uds.com.br>
2021-08-23 08:44:23 -0300

    criando login controller

--- lib/app/modules/auth/login/login_controller.dart
+++ lib/app/modules/auth/login/login_controller.dart
@@ -1,3 +1,27 @@
-import 'package:flutter/material.dart';
+import 'package:todo_list/app/core/notifier/app_change_notifier.dart';
+import 'package:todo_list/app/exception/auth_exception.dart';
+import 'package:todo_list/app/service/user/user_service_interface.dart';
 
-class LoginController extends ChangeNotifier {}
+class LoginController extends AppChangeNotifier {
+  final IUserService _userService;
+
+  LoginController({required IUserService userService}) : _userService = userService;
+
+  Future<void> login(String email, String password) async {
+    try {
+      showLoadingAndResetState();
+      notifyListeners();
+      final user = await _userService.login(email, password);
+      if (user != null) {
+        success();
+      } else {
+        setError("Usuario ou senha inválidos");
+      }
+    } on AuthException catch (e) {
+      setError(e.message);
+    } finally {
+      hideLoading();
+      notifyListeners();
+    }
+  }
+}
