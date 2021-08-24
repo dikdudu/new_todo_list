3c73b4b7344ca888fa0896ffde77055bc09e360c
CaduUds <eduardo.santos@uds.com.br>
2021-08-24 16:31:21 -0300

    adicionando rotas do tasks_modules

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -5,6 +5,7 @@ import 'package:todo_list/app/core/ui/todo_list_ui_config.dart';
 import 'package:todo_list/app/modules/auth/auth_module.dart';
 import 'package:todo_list/app/modules/home/home_module.dart';
 import 'package:todo_list/app/modules/splash/splash_page.dart';
+import 'package:todo_list/app/modules/tasks/tasks_module.dart';
 
 class AppWidget extends StatefulWidget {
   AppWidget({Key? key}) : super(key: key);
@@ -38,6 +39,7 @@ class _AppWidgetState extends State<AppWidget> {
       routes: {
         ...AuthModule().routers,
         ...HomeModule().routers,
+        ...TasksModule().routers,
       },
       home: SplashPage(),
     );
